d2ac7a2821a558e848cc2cbf600a70a8fdd647a4
egutierrez112999 <egutierrez112999@gmail.com>
2023-04-29 11:43:08 -0600

    created Awrds1 query

--- Database.py
+++ Database.py
@@ -79,7 +79,8 @@ class Database:
         return self.cursor.fetchall()
 
     def GetAwards1(self): # Awards for the survival genre from creators that do not have a degree
-        pass
+        self.cursor.execute("SELECT awardName, games.gameName, genre, creators.creatorName FROM awards JOIN games ON awards.gameName = games.gameName JOIN creators ON games.creatorName = creators.creatorName WHERE degree = 0 AND genre = 'Survival'")
+        return self.cursor.fetchall()
 
     def GetGames1(self): #Games with a creator who has a degree and at least 2 awards
         pass
@@ -87,7 +88,7 @@ class Database:
 db = Database()
 db.AnnihilateTables()
 db.CreateTables()
-db.CreateManyCreators(20)
-db.CreateGames(40)
-db.CreateAwards(5)
-print(db.GetCreators1())
+db.CreateManyCreators(100)
+db.CreateGames(200)
+db.CreateAwards(20)
+print(db.GetAwards1())
